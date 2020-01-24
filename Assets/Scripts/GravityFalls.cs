1f9ee142210e32f596a2124b498503c8024644ad
jmacnut <jmacnut@gmail.com>
2020-01-24 02:54:19 -1000

    completes challenge 6

---
+++ Assets/Scripts/GravityFalls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityFalls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Rigidbody rb = this.GetComponent<Rigidbody>();
+
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+         GameObject.Find("Floor").GetComponent<MeshRenderer>().material.color = Color.red;
+         rb.useGravity = true;
+      }
+   }
+
+   private void OnTriggerEnter(Collider other)
+   {
+      other.GetComponent<MeshRenderer>().material.color = Color.blue;
+      Destroy(this.gameObject);
+   }
+}
