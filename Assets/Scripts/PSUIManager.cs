af5cd58ffa4a23e3a9d26ff135266ffd78205c5f
jmacnut <jmacnut@gmail.com>
2020-01-24 14:29:26 -1000

    completes challenge 7

---
+++ Assets/Scripts/PSUIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PSUIManager : MonoBehaviour
+{
+   [SerializeField]
+   private PlayerStats playerStats;
+
+   [SerializeField]
+   private Text playerName;
+   [SerializeField]
+   private Text attackLevel;
+   [SerializeField]
+   private Text magicLevel;
+   [SerializeField]
+   private Text smithingLevel;
+   [SerializeField]
+   private Text miningLevel;
+
+   private void Start()
+   {
+      playerStats = GameObject.Find("Player").GetComponent<PlayerStats>();
+      //textField = GameObject.Find("Player_Name_Text").GetComponent<Text>();
+      playerName.text = "Player Name: " + playerStats.playerName;
+      attackLevel.text = "Attack Level: " + playerStats.attackLevel;
+      magicLevel.text = "Magic Level: " + playerStats.magicLevel;
+      smithingLevel.text = "Smithing Level: " + playerStats.smithingLevel;
+      miningLevel.text = "Mining Level: " + playerStats.miningLevel;
+   }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public string playerName;
+    public int attackLevel;
+    public int magicLevel;
+    public int smithingLevel;
+    public int miningLevel;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
