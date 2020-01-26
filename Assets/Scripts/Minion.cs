007a30669dc759e70b31de7e1eac59841881fad3
jmacnut <jmacnut@gmail.com>
2020-01-25 17:02:30 -1000

    completes challenge 9

---
+++ Assets/Scripts/Minion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minion : MonoBehaviour 
+{
+   public float _speed = 0.1f;
+   float xpos;
+   float ypos;
+   float zpos;
+
+   void Start () {
+      xpos = transform.position.x;
+      ypos = transform.position.y;
+      zpos = transform.position.z;
+   }
+	
+	void Update () {
+
+      Vector3 currPos = new Vector3(xpos++, -2.384f, 0);
+      transform.position = currPos;
+
+      if (xpos >= 6.63)
+      {
+         Destroy(this.gameObject);
+      }
+   }
+}
---
+++ Assets/Scripts/MinionSpawnManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionSpawnManager : MonoBehaviour
+{
+   public GameObject minionPrefab;
+   public Transform minionTransform;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+         Instantiate(minionPrefab, minionTransform);
+      }
+   }
+}
