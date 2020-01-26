e7d2578a8177174f19305fee3215e48641ac46e1
jmacnut <jmacnut@gmail.com>
2020-01-26 00:53:10 -1000

    completes challenge 11

---
+++ Assets/Scripts/ThreeRedCubes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreeRedCubes : MonoBehaviour
+{
+   public GameObject[] cubes;
+
+   void Start()
+   {
+      cubes = GameObject.FindGameObjectsWithTag("Cube");
+   }
+
+   void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+         foreach (var cube in cubes)
+         {
+            cube.GetComponent<MeshRenderer>().material.color = Color.red;
+         }
+      }
+   }
+}
