7d8d70946e156d369d093b737f076537916246ac
jmacnut <jmacnut@gmail.com>
2020-01-25 17:22:43 -1000

    completes challenge 10

---
+++ Assets/Scripts/TellMeTheNames.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TellMeTheNames : MonoBehaviour
+{
+   [SerializeField]
+   private List<string> _namesList;
+
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+         foreach (var name in _namesList)
+         {
+            Debug.Log("Name: " + name);
+         }
+      }
+   }
+}
