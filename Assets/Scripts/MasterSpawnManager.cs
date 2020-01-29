cfb120697b36f339f7c75828208e82dd2dc14b13
jmacnut <jmacnut@gmail.com>
2020-01-28 16:59:24 -1000

    completes challenge 12

---
+++ Assets/Scripts/MasterSpawnManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterSpawnManager : MonoBehaviour
+{
+   [SerializeField]
+   private List<GameObject> objectsToSpawn = new List<GameObject>();
+
+   [SerializeField]
+   private Transform spawnedContainer;
+
+   [SerializeField]
+   private GameObject cubePrefab;
+
+   [SerializeField]
+   private GameObject spherePrefab;
+
+   [SerializeField]
+   private GameObject capsulePrefab;
+
+   [SerializeField]
+   private GameObject cylinderPrefab;
+
+   private int timeToLive = 2;
+
+   private void Start()
+   {
+      objectsToSpawn.Add(cubePrefab);
+      objectsToSpawn.Add(spherePrefab);
+      objectsToSpawn.Add(capsulePrefab);
+      objectsToSpawn.Add(cylinderPrefab);
+
+      StartCoroutine(SpawnTimerRoutine());
+   }
+
+   private void Update()
+   {
+
+   }
+
+   IEnumerator SpawnTimerRoutine()
+   {
+      while (true)
+      {
+         StartCoroutine(ObjectSpawnerRoutine());
+         yield return new WaitForSeconds(timeToLive);
+      }
+   }
+
+   IEnumerator ObjectSpawnerRoutine()
+   {
+      List<GameObject> spawnedObjects = new List<GameObject>();
+
+
+      foreach (var obj in objectsToSpawn)
+      {
+         int index = Random.Range(0, 3);
+
+         float xpos = Random.Range(-6f, 6f);
+         float ypos = Random.Range(-3f, 3f);
+         float zpos = Random.Range(-3f, 3f);
+
+         Vector3 newPos = new Vector3(xpos, ypos, zpos);
+
+         spawnedObjects.Add(Instantiate(objectsToSpawn[index], newPos, Quaternion.identity, spawnedContainer));
+
+      }
+
+      yield return new WaitForSeconds(timeToLive);
+
+      // collect SpawnedItem and destroy them
+      foreach (var obj in spawnedObjects)
+      {
+         Destroy(obj);
+      }
+
+   }
+
+}
---
+++ Assets/Scripts/ObjectController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   //IEnumerator ObjectSpawnerRoutine()
+   //{
+   //   int index = Random.Range(0, 3);
+
+   //   Instantiate(objectsToSpawn[index]);
+
+   //   yield return new WaitForSeconds(timeToLive);
+
+   //   Destroy(this.gameObject);
+   //}
+}
